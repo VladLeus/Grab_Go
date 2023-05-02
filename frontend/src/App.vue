f263ebd8761020ad49b85c3eb34115e04096105c
VladLeus <vladyslavleus@gmail.com>
2023-05-02 21:00:01 +0300

    Added mobile navigation.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
+    <div class="font-RobotoSlab bg-white">
+        <NavBar />
         <RouterView />
     </div>
 </template>
 
 <script setup>
 import {RouterView} from "vue-router";
+import NavBar from "@/components/NavBar.vue";
 </script>
